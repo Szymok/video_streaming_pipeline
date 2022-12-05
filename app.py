115d66b7f9f99b6f6e32a52d92c20c2192bdd80c
Szymon Kowalewski <37596854+Szymok@users.noreply.github.com>
2022-12-05 23:02:30 +0000

    create flask app

--- app.py
+++ app.py
@@ -56,3 +56,49 @@ def query_model(video_path):
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
+
+
+# Flask APP
+@app.route('/')
+def index():
+	return render_template('camera.html')
+
+
+@socketio.on('connection_msg')
+def connected(message):
+	print('Connected')
+	data = message
+
+
+@socketio.on('train_img')
+def main1(message):
+	# message contains the image from the client side and the type of ml service to use
+	# label detection or text extraction
+	# data = np.array(list(message.values()))
+
+	# The type of ml service to use - stored in image_analysis
+	# image_analysis = data[0][0]
+
+	# The image to encode - from the client side
+	# img_to_encode = data[0][1]
+	# print(img_to_encode)
+
+	# Then split the list and encode everything after the ',' and store in img_encoded
+	# img_encoded = img_to_encode.split(',')[1].encode()
+	print(len(message['data'][0]))
+	data = message['data'][0]
+
+	# Get Current ts in ms- append timestamp to image name so we always save a new image
+	timestamp = datetime.now()
+	time_formatted = timestamp.strftime('%Y%m%d%H%M%S')
+	file_name = 'pic_' + time_formatted + '.webm'
+
+	# Folder to store the image
+	folder = '/home/skszymon/videos/'
+	uri = folder + file_name
+
+	# Need to decode it using base64
+	with open(uri, 'wb') as fh:
+		fh.write(data)
+	os.system('webm -i ' + uri + ' ' + folder + 'pic_' + time_formatted + '.mp4')
+	os.system('rm ' + uri)
