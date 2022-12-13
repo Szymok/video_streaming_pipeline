23d296de62ade1bfb4bd6ff9cdd13b23bf36c66b
Szymon Kowalewski <37596854+Szymok@users.noreply.github.com>
2022-12-13 21:52:02 +0000

    HogTransformer

--- train.py
+++ train.py
@@ -17,6 +17,7 @@ from skimage import color
 from skimage.feature import hog
 from urllib.parse import urlparse
 
+
 class RGB2GrayTransformer(BaseEstimator, TransformerMixin):
 	'''
  	Convert array of RGB images to grayscale
@@ -30,7 +31,48 @@ class RGB2GrayTransformer(BaseEstimator, TransformerMixin):
 
 	def transform(self, X, y=None):
 		# print(type(X))
-		if type(X)!=list and type(X)!=np.ndarray:
-			img=np.asarray(X.inputs[0].data).reshape(224,224,3)
+		if type(X) != list and type(X) != np.ndarray:
+			img = np.asarray(X.inputs[0].data).reshape(224, 224, 3)
 			print(img.shape)
 			return np.array([color.rgb2gray(img) for img in X])
+
+
+class HogTransformer(BaseEstimator, TransformerMixin):
+	'''
+ 	Expects an array of 2d arrays
+	Calculates hog features for each img
+	'''
+
+	def __init__(self,
+	             y=None,
+	             orientations=9,
+	             pixels_per_cell=(8, 8),
+	             cells_per_block=(3, 3),
+	             block_norm='L2-Hys'):
+		self.y = y
+		self.orientations = orientations
+		self.pixels_per_cell = pixels_per_cell
+		self.cells_per_block = cells_per_block
+		self.block_norm = block_norm
+
+	def fit(self, X, y=None):
+		return self
+
+	def transform(self, X, y=None):
+
+		def local_hog(X):
+			return hog(X,
+			           orientation=self.orientations,
+			           pixels_per_cell=self.pixels_per_cell,
+			           cells_per_block=self.cells_per_block,
+			           block_norm=self.block_norm)
+
+		if X.shape == (224, 244):
+			X = X.reshape(224, 224)
+			return np.array(local_hog(X)).reshape(1, 23328)
+		else:
+			try:
+				print(np.array([local_hog(img) for img in X]).shape)
+				return np.array([local_hog(img) for img in X])
+			except:
+				return np.array([local_hog(img) for img in X])
